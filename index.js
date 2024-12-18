2eff01c202b468858b62c05f99fd20ad7ad47a94
Sankeerthi-m <sankeerthicreations@gmail.com>
2024-12-18 14:31:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var http = require('http');
+// Create a server object:http.createServer(function (req, res) {
+  res.write('Welcome to DevOps Training'); // Write a response to the client  res.end(); // End the response
+}).listen(3000, '0.0.0.0', () => {  console.log('Server is running on port 3000');
+}); // The server listens on port 3000 and all network interfaces
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Welcome to DevOps Training 2024'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
